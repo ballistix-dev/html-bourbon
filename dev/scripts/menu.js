4adde2d0e33e3e5c4a44e3eb3a1c44c215814650
Marcel Badua <marcelbadua@Marcels-MacBook-Pro.local>
2017-02-07 19:32:11 +0800

    1.0.1

--- dev/scripts/menu.js
+++ dev/scripts/menu.js
@@ -1,18 +1,18 @@
-// $(function() {
-// 	$('.menu-item-has-children > a').on('click', function(event){
-// 		event.stopPropagation();
-// 		event.preventDefault();	
-// 	});
-// 	$('.menu-item-has-children > a').on('click', function(event){
-// 		if ( !($(this).hasClass('is-active')) ) {
-// 			$(this).closest('ul').find('.is-active').removeClass('is-active');
-// 		}
-// 		$(this).toggleClass('is-active');
-// 		$(this).parent().toggleClass('is-active');
-// 	})
-// 	$('html').on('click', function(){	
-// 		$('.menu').find('.is-active').each(function(){
-// 			$(this).removeClass('is-active');
-// 		});	
-// 	})
-// });
+$(function() {
+	$('.menu-item-has-children > a').on('click', function(event){
+		event.stopPropagation();
+		event.preventDefault();	
+	});
+	$('.menu-item-has-children > a').on('click', function(event){
+		if ( !($(this).hasClass('is-active')) ) {
+			$(this).closest('ul').find('.is-active').removeClass('is-active');
+		}
+		$(this).toggleClass('is-active');
+		$(this).parent().toggleClass('is-active');
+	})
+	$('html').on('click', function(){	
+		$('.menu').find('.is-active').each(function(){
+			$(this).removeClass('is-active');
+		});	
+	})
+});
