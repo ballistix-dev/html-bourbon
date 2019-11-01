e57206e24882e2e396bc5c9b1a382be54e39a834
Marcel Badua <bitterdash@gmail.com>
2019-11-01 09:51:13 +0800

    2.0.5

--- dev/scripts/toggle-menu.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  $('*[data-toggle="toggle-menu"]').on('click', function(event) {
-      event.preventDefault();
-      var target = $(this).data('target');
-      $(this).toggleClass('is-active');
-      $(target).find('.menu').toggleClass('is-active');
-  });
-});
